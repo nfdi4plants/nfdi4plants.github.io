5c351cf45da6043c88c96ced28e0d3e1c00c6310
CMR248 <cristina.martins.rodrigues@rz.uni-freiburg.de>
2021-09-09 18:36:01 +0200

    Change in News-Hero

--- src/loaders/globalloader.fsx
+++ src/loaders/globalloader.fsx
@@ -6,6 +6,6 @@ type SiteInfo = {
 }
 
 let loader (projectRoot: string) (siteContent: SiteContents) =
-    siteContent.Add({title = "nfdi4plants static site template"; description = "static webpages with nfdi4plants branding created with fornax and bulma sass"})
+    siteContent.Add({title = "nfdi4plants"; description = "DataPLANT News"})
 
     siteContent
